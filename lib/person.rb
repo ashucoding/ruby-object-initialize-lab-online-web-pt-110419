cd89c01a5b7b870759fb58d1c4d41e90d9bf1fec
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-30 02:23:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,14 @@
 class Person 
+  def initialize (persons_name)
+    @name = persons_name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
   
+end
